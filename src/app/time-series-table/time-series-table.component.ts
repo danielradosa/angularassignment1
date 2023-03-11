e94577870cc3ce83c968da125fcf816a92ed8fc7
Daniel Radosa <danysko5@gmail.com>
2023-03-11 14:01:51 +0100

    working value editing

--- src/app/time-series-table/time-series-table.component.ts
+++ src/app/time-series-table/time-series-table.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../data.service';
+import { IData } from '../interface';
 
 @Component({
   selector: 'app-time-series-table',
@@ -7,13 +8,23 @@ import { DataService } from '../data.service';
   styleUrls: ['./time-series-table.component.css']
 })
 export class TimeSeriesTableComponent {
-  data: any[];
+  datas: IData[] = [];
 
   constructor(private dataService: DataService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.dataService.getData().subscribe(data => {
-      this.data = data;
+      this.datas = data;
     });
+  }
+
+  openEditDialog(item: IData): void {
+    const value = prompt(`Enter new value for ${item.time}:`, item.value.toString());
+    if (value !== null) {
+      const newValue = parseFloat(value);
+      if (!isNaN(newValue)) {
+        item.value = newValue;
+      }
+    }
   }
 }
